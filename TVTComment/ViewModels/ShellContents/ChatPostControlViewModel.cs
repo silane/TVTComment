03f5867fd705ba570ef0e0c77fb7f029354456f1
silane <18604696+silane@users.noreply.github.com>
2020-12-10 16:10:38 +0900

    [controller] ChatPostControlViewModelのリファクタリング

--- TVTComment/ViewModels/ShellContents/ChatPostControlViewModel.cs
+++ TVTComment/ViewModels/ShellContents/ChatPostControlViewModel.cs
@@ -11,37 +11,21 @@ using System.Windows.Input;
 
 namespace TVTComment.ViewModels.ShellContents
 {
-    class ChatPostControlViewModel:INotifyPropertyChanged
+    class ChatPostControlViewModel : INotifyPropertyChanged
     {
         private Model.TVTComment model;
 
-        private ObservableValue<byte> chatOpacity;
-        public ObservableValue<byte> ChatOpacity => chatOpacity ?? (chatOpacity = model?.ChatOpacity?.MakeLinkedObservableValue(x => (byte)(x / 16), x => (byte)(x * 16)));
+        public ObservableValue<byte> ChatOpacity { get; private set; }
         public ObservableValue<string> PostText { get; } = new ObservableValue<string>("");
         public ObservableValue<string> PostMailText { get; } = new ObservableValue<string>("");
-
-        private DisposableReadOnlyObservableCollection<Model.ChatCollectService.IChatCollectService> postServices;
-        public DisposableReadOnlyObservableCollection<Model.ChatCollectService.IChatCollectService> PostServices => 
-            postServices ?? (postServices = model.ChatCollectServiceModule?.RegisteredServices?.MakeOneWayLinkedCollectionMany(x =>
-        {
-            if (x.CanPost) return new[] { x };
-            else return new Model.ChatCollectService.IChatCollectService[0];
-        }));
+        public DisposableReadOnlyObservableCollection<Model.ChatCollectService.IChatCollectService> PostServices { get; private set; }
         public ObservableValue<Model.ChatCollectService.IChatCollectService> SelectedPostService { get; } = new ObservableValue<Model.ChatCollectService.IChatCollectService>();
-        public ReadOnlyObservableValue<bool> IsShowingNiconicoPostForm => new ReadOnlyObservableValue<bool>(
-            this.SelectedPostService.Select(x => x is Model.ChatCollectService.NiconicoChatCollectService)
-        );
-
+        public ReadOnlyObservableValue<bool> IsShowingNiconicoPostForm { get; private set; }
         public ObservableCollection<string> PostMailTextExamples => model.ChatPostMailTextExamples;
 
-        private ICommand postCommand;
-        public ICommand PostCommand => postCommand ?? (postCommand = new DelegateCommand(PostChat));
-
-        private ICommand addPostMailTextExampleCommand;
-        public ICommand AddPostMailTextExampleCommand => addPostMailTextExampleCommand ?? (addPostMailTextExampleCommand = new DelegateCommand<string>(AddPostMailTextExample));
-
-        private ICommand removePostMailTextExampleCommand;
-        public ICommand RemovePostMailTextExampleCommand => removePostMailTextExampleCommand ?? (removePostMailTextExampleCommand = new DelegateCommand<string>(RemovePostMailTextExample));
+        public ICommand PostCommand { get; private set; }
+        public ICommand AddPostMailTextExampleCommand { get; private set; }
+        public ICommand RemovePostMailTextExampleCommand { get; private set; }
 
         public InteractionRequest<Notification> AlertRequest { get; } = new InteractionRequest<Notification>();
 
@@ -50,12 +34,27 @@ namespace TVTComment.ViewModels.ShellContents
         public ChatPostControlViewModel(Model.TVTComment model)
         {
             this.model = model;
-            Initialize();
+            this.model.Initialize().ContinueWith(task => {
+                if (!task.IsCompletedSuccessfully) return;
+                this.initialize();
+            }, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
-        public async Task Initialize()
+        private void initialize()
         {
-            await model.Initialize();
+            this.ChatOpacity = model.ChatOpacity.MakeLinkedObservableValue(x => (byte)(x / 16), x => (byte)(x * 16));
+            this.PostServices = model.ChatCollectServiceModule?.RegisteredServices?.MakeOneWayLinkedCollectionMany(x => {
+                if (x.CanPost) return new[] { x };
+                else return new Model.ChatCollectService.IChatCollectService[0];
+            });
+            this.IsShowingNiconicoPostForm = new ReadOnlyObservableValue<bool>(
+                this.SelectedPostService.Select(x => x is Model.ChatCollectService.NiconicoChatCollectService)
+            );
+
+            this.PostCommand = new DelegateCommand(PostChat);
+            this.AddPostMailTextExampleCommand = new DelegateCommand<string>(AddPostMailTextExample);
+            this.RemovePostMailTextExampleCommand = new DelegateCommand<string>(RemovePostMailTextExample);
+
             PropertyChanged(this, new PropertyChangedEventArgs(null));
         }
 
