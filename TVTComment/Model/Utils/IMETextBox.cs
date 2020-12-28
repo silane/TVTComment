b2837f8356655621656a92e806c755639f25322d
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2020-12-29 07:51:51 +0900

    TextBoxのUpdateSourceTriggerがIME変換時に反応するバグを回避？

---
+++ TVTComment/Model/Utils/IMETextBox.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Controls;
+
+namespace TVTComment.Model.Utils
+{
+    public class IMETextBox : TextBox
+    {
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            GetBindingExpression(TextProperty).UpdateSource();
+            base.OnTextChanged(e);
+        }
+    }
+}
