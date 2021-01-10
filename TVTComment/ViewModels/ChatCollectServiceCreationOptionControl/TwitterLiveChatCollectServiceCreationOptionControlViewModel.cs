0253c105cf5b1ac689921a01984ceb1b753057bf
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2021-01-10 14:18:13 +0900

    Twitter用のダイアログ追加

---
+++ TVTComment/ViewModels/ChatCollectServiceCreationOptionControl/TwitterLiveChatCollectServiceCreationOptionControlViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Commands;
+using System;
+using System.Windows.Input;
+using TVTComment.Model.ChatCollectServiceEntry;
+
+namespace TVTComment.ViewModels.ChatCollectServiceCreationOptionControl
+{
+    class TwitterLiveChatCollectServiceCreationOptionControlViewModel : ChatCollectServiceCreationOptionControlViewModel
+    {
+        private string searchWord;
+
+        public string SearchWord
+        {
+            get { return this.searchWord; }
+            set { SetProperty(ref this.searchWord, value); }
+        }
+
+        public ICommand OkCommand { get; }
+
+        public override event EventHandler Finished;
+
+        public TwitterLiveChatCollectServiceCreationOptionControlViewModel()
+        {
+            this.OkCommand = new DelegateCommand(() => this.Finished(this, new EventArgs()));
+        }
+
+        public override IChatCollectServiceCreationOption GetChatCollectServiceCreationOption()
+        {
+            if (string.IsNullOrWhiteSpace(this.searchWord))
+                return null;
+            return new TwitterLiveChatCollectServiceEntry.ChatCollectServiceCreationOption(this.searchWord);
+        }
+    }
+}
---
+++ TVTComment/Views/ChatCollectServiceCreationOptionControl/TwitterLiveChatCollectServiceCreationOptionControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TVTComment.Views.ChatCollectServiceCreationOptionControl
+{
+    /// <summary>
+    /// NiconicoLiveChatCollectServiceCreationOptionControl.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TwitterLiveChatCollectServiceCreationOptionControl : UserControl
+    {
+        public TwitterLiveChatCollectServiceCreationOptionControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
