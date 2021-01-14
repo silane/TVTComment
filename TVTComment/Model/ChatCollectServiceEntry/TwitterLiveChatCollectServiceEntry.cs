7d11e1fc8c60af2084a9b9fa777516a590ca1685
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2021-01-14 15:32:08 +0900

    Twitter実況用のEntryをとりあえず追加

---
+++ TVTComment/Model/ChatCollectServiceEntry/TwitterLiveChatCollectServiceEntry.cs
@@ -0,0 +1,37 @@
+﻿using ObservableUtils;
+using System;
+using TVTComment.Model.TwitterUtils;
+
+namespace TVTComment.Model.ChatCollectServiceEntry
+{
+    class TwitterLiveChatCollectServiceEntry : IChatCollectServiceEntry
+    {
+        public class ChatCollectServiceCreationOption : IChatCollectServiceCreationOption
+        {
+            public string SearchWord { get; }
+            public ChatCollectServiceCreationOption(string searchWord)
+            {
+                this.SearchWord = searchWord;
+            }
+        }
+
+        public ChatService.IChatService Owner { get; }
+        public string Id => "TwitterLive";
+        public string Name => "Twitterリアルタイム実況";
+        public string Description => "指定した検索ワードでTwitter実況";
+        public bool CanUseDefaultCreationOption => false;
+
+        private ObservableValue<TwitterAuthentication> Session;
+
+        public TwitterLiveChatCollectServiceEntry(ChatService.TwitterChatService Owner, ObservableValue<TwitterAuthentication> Session)
+        {
+            this.Owner = Owner;
+            this.Session = Session;
+        }
+
+        public ChatCollectService.IChatCollectService GetNewService(IChatCollectServiceCreationOption creationOption)
+        {
+            return null;
+        }
+    }
+}
