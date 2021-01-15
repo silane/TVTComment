b7522f164749f0b0524ad55de674945ab3522e5d
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2021-01-15 13:00:21 +0900

    StatusからChatに変換するコンバーター実装

---
+++ TVTComment/Model/TwitterUtils/ChatTwitterStatusToChat.cs
@@ -0,0 +1,18 @@
+﻿using CoreTweet;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVTComment.Model.TwitterUtils
+{
+    class ChatTwitterStatusToChat
+    {
+        public static Chat Convert(Status status)
+        {
+            return new Chat(status.CreatedAt.DateTime ,status.Text, Chat.PositionType.Normal, Chat.SizeType.Normal, Color.FromArgb(255, 255, 255),status.User.ScreenName,(int)status.Id);
+        }
+    }
+}
