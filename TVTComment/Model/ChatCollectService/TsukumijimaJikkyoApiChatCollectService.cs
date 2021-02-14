be1d31bdde0a1d1714c8694b9c9f9f9defbf4605
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2021-02-15 01:12:26 +0900

    削除されたコメがあると不正な応答で過去ログ取得できないのを修正

--- TVTComment/Model/ChatCollectService/TsukumijimaJikkyoApiChatCollectService.cs
+++ TVTComment/Model/ChatCollectService/TsukumijimaJikkyoApiChatCollectService.cs
@@ -177,6 +177,9 @@ namespace TVTComment.Model.ChatCollectService
 
                     if (threadNoList.Contains((thread, no))) // 同じスレッドの同じコメ番があればスキップ
                         continue;
+                    
+                    if (chatObj.TryGetProperty("deleted", out _)) //削除済みの場合スキップ
+                        continue;
 
                     int vpos = int.Parse(chatObj.GetProperty("vpos").GetString());
                     long date = int.Parse(chatObj.GetProperty("date").GetString());
