9713488fd9665499e541b395ff5d31ef7fcc8894
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2021-02-18 20:49:35 +0900

    Annict用のException

---
+++ TVTComment/Model/TwitterUtils/AnnictUtils/AnnictException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TVTComment.Model.TwitterUtils.AnnictUtils
+{
+    class AnnictException : InvalidOperationException
+    {
+        public AnnictException()
+        {
+        }
+
+        public AnnictException(string message) : base(message)
+        {
+        }
+
+        public AnnictException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+    class AnnictResponseException : AnnictException
+    {
+        public AnnictResponseException()
+        {
+        }
+
+        public AnnictResponseException(string message) : base(message)
+        {
+        }
+
+        public AnnictResponseException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
