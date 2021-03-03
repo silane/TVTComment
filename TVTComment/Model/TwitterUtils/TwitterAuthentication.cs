f3866c93ba6d4d81aab26f1ca035f805463aa3f1
noriokun4649 <souta46493yoroosiku@yahoo.co.jp>
2021-03-04 07:41:33 +0900

    認証系にAPI V2用の処理追加

--- TVTComment/Model/TwitterUtils/TwitterAuthentication.cs
+++ TVTComment/Model/TwitterUtils/TwitterAuthentication.cs
@@ -1,5 +1,7 @@
 ﻿using CoreTweet;
+using CoreTweet.Core;
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using static CoreTweet.OAuth;
 
@@ -25,11 +27,13 @@ namespace TVTComment.Model.TwitterUtils
     class TwitterAuthentication
     {
         private Tokens token = null;
+        private OAuth2Token oAuth2Token = null;
         public bool IsLoggedin => token != null;
         private readonly string apikey;
         private readonly string apiseecret;
         private readonly string apiaccesstoken;
         private readonly string apiaccesssecret;
+        private readonly string bearertoken;
         private readonly OAuthSession authSession;
 
         public Tokens Token
@@ -37,6 +41,13 @@ namespace TVTComment.Model.TwitterUtils
             get
             {
                 return token;
+            }
+        }
+        public OAuth2Token OAuth2Token
+        {
+            get
+            {
+                return oAuth2Token;
             }
         }
 
@@ -54,12 +65,13 @@ namespace TVTComment.Model.TwitterUtils
             authSession = Authorize(apikey, apiseecret);
         }
 
-        public TwitterAuthentication(string apikey, string apiseecret, string apiaccesstoken, string apiaccesssecret)
+        public TwitterAuthentication(string apikey, string apiseecret, string apiaccesstoken, string apiaccesssecret, string bearertoken)
         {
             this.apikey = apikey;
             this.apiseecret = apiseecret;
             this.apiaccesstoken = apiaccesstoken;
             this.apiaccesssecret = apiaccesssecret;
+            this.bearertoken = bearertoken;
         }
 
         public void Login()
@@ -69,6 +81,9 @@ namespace TVTComment.Model.TwitterUtils
             if (string.IsNullOrWhiteSpace(apiaccesssecret) && string.IsNullOrWhiteSpace(apiaccesstoken))
                 throw new InvalidOperationException("Access tokenとAccess seecretが空白もしくは問題があります。");
             token = Tokens.Create(apikey, apiseecret, apiaccesstoken, apiaccesssecret);
+            if (!bearertoken.Equals(""))
+                oAuth2Token = OAuth2Token.Create(apikey, apiseecret, bearertoken);
+            Debug.WriteLine(oAuth2Token.BearerToken);
         }
 
         public async Task Login(string pin)
@@ -78,6 +93,10 @@ namespace TVTComment.Model.TwitterUtils
             if (string.IsNullOrWhiteSpace(pin))
                 throw new InvalidOperationException("PINが空白もしくは問題があります。");
             token = await OAuth.GetTokensAsync(authSession, pin);
+            oAuth2Token = await OAuth2.GetTokenAsync(apikey, apiseecret);
+            if (oAuth2Token.BearerToken != null && !oAuth2Token.BearerToken.Equals(""))
+                oAuth2Token = OAuth2Token.Create(apikey, apiseecret, oAuth2Token.BearerToken);
+            Debug.WriteLine(oAuth2Token.BearerToken);
         }
 
         public void Logout()
@@ -85,6 +104,7 @@ namespace TVTComment.Model.TwitterUtils
             if (!IsLoggedin)
                 throw new InvalidOperationException("ログインしていません");
             token = null;
+            oAuth2Token = null;
         }
 
     }
