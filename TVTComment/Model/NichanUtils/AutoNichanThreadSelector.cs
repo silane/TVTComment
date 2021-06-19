6833e375a822d64c2d89fa0effffd81b56eaadff
Nep <spica@starry.blue>
2021-06-19 21:54:32 +0900

    2chthreads.txt でチャンネルの板定義を複数設定できるようにした

--- TVTComment/Model/NichanUtils/AutoNichanThreadSelector.cs
+++ TVTComment/Model/NichanUtils/AutoNichanThreadSelector.cs
@@ -24,36 +24,41 @@ namespace TVTComment.Model.NichanUtils
             ChannelInfo channel, DateTimeOffset time, CancellationToken cancellationToken
         )
         {
-            MatchingThread matchingThread = threadResolver.Resolve(channel, false);
-            if (matchingThread == null)
-                return Array.Empty<string>();
+            IEnumerable<MatchingThread> matchingThreads = threadResolver.Resolve(channel, false);
+            List<string> threads = new List<string>();
 
-            IEnumerable<string> keywords = matchingThread.ThreadTitleKeywords.Select(
-                x => x.ToLower().Normalize(NormalizationForm.FormKD)
-            );
+            foreach (var entry in matchingThreads)
+            {
+                IEnumerable<string> keywords = entry.ThreadTitleKeywords.Select(
+                    x => x.ToLower().Normalize(NormalizationForm.FormKD)
+                );
 
-            string boardUri = matchingThread.BoardUri.ToString();
-            var uri = new Uri(boardUri);
-            string boardHost = $"{uri.Scheme}://{uri.Host}";
-            string boardName = uri.Segments[1];
-            if (boardName.EndsWith('/'))
-                boardName = boardName[..^1];
+                string boardUri = entry.BoardUri.ToString();
+                var uri = new Uri(boardUri);
+                string boardHost = $"{uri.Scheme}://{uri.Host}";
+                string boardName = uri.Segments[1];
+                if (boardName.EndsWith('/'))
+                    boardName = boardName[..^1];
 
-            byte[] subjectBytes = await httpClient.GetByteArrayAsync(
-                $"{boardHost}/{boardName}/subject.txt", cancellationToken
-            );
-            string subject = Encoding.GetEncoding(932).GetString(subjectBytes);
+                byte[] subjectBytes = await httpClient.GetByteArrayAsync(
+                    $"{boardHost}/{boardName}/subject.txt", cancellationToken
+                );
+                string subject = Encoding.GetEncoding(932).GetString(subjectBytes);
 
-            using var textReader = new StringReader(subject);
-            IEnumerable<Nichan.Thread> threadsInBoard = await Nichan.SubjecttxtParser.ParseFromStream(textReader);
+                using var textReader = new StringReader(subject);
+                IEnumerable<Nichan.Thread> threadsInBoard = await Nichan.SubjecttxtParser.ParseFromStream(textReader);
 
-            return threadsInBoard.Select(
-                x => { x.Title = x.Title.ToLower().Normalize(NormalizationForm.FormKD); return x; }
-            ).Where(
-                x => x.ResCount <= 1000 && keywords.All(keyword => x.Title.Contains(keyword))
-            ).OrderByDescending(x => x.ResCount).Take(3).Select(
-                x => $"{boardHost}/test/read.cgi/{boardName}/{x.Name}/l50"
-            );
+                var urls = threadsInBoard.Select(
+                    x => { x.Title = x.Title.ToLower().Normalize(NormalizationForm.FormKD); return x; }
+                ).Where(
+                    x => x.ResCount <= 1000 && keywords.All(keyword => x.Title.Contains(keyword))
+                ).OrderByDescending(x => x.ResCount).Take(3).Select(
+                    x => $"{boardHost}/test/read.cgi/{boardName}/{x.Name}/l50"
+                );
+                threads.AddRange(urls);
+            }
+
+            return threads;
         }
     }
 }
