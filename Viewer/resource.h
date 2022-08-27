19694fa1f7df3141b1c01d3e463b4c57268d1b0b
noriokun4649 <noriokun.official.mail@gmail.com>
2022-08-28 05:34:10 +0900

    Add Resource.

--- Viewer/resource.h
+++ Viewer/resource.h
@@ -2,6 +2,9 @@
 #define IDC_STATIC (-1)
 #endif
 
-/* #define IDB_ICON                                110 */
-/* #define IDB_COMMENT                             114 */
-/* #define IDB_COMMENT16                           115 */
+#define IDB_TVTCICON							116
+#define IDB_TVTCTOPICON							117
+#define IDB_TVTCVIEWICON						118
+#define IDB_TVTCICON16							119
+#define IDB_TVTCTOPICON16						120
+#define IDB_TVTCVIEWICON16						121
